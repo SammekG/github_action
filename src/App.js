5a81501cd86f5122f17f0ffaaaf2179c6ceb854f
SammekG <sammek.gandhi@gmail.com>
2024-08-22 15:00:43 +0530

    Fina Changes

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           {process.env.REACT_APP_WELCOME_TEXT || "Github Actions Tutorial"} - By
           Sammek Gandhi
